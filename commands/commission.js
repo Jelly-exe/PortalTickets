ce8164e24b83354f62b7b8b3ced3a07ff041ea73
BuildTools <unconfigured@null.spigotmc.org>
2019-08-04 01:52:36 +0100

    Fixed bugs with commission.js Added 'Extra details' to commission system Corrected reaction so they did correct things Made reactions appear in correct order Updated NPM files

--- commands/commission.js
+++ commands/commission.js
@@ -5,55 +5,58 @@ const delay = require('delay');
 const config = require("../config-files/main.json");
 
 module.exports.run = async (client, message, args) => {
-  
+
   const ended = new Discord.RichEmbed()
   .setDescription(':x: Commission creationed timed out!')
   .setColor(config.colour);
-  
+
+  const ended2 = new Discord.RichEmbed()
+  .setDescription(':x: Role not found!')
+  .setColor(config.colour);
+
   message.delete();
   const embed = new Discord.RichEmbed()
       .setTitle('Commission Creation System')
       .setDescription('Please use the instuctions below to create the commission:\n\n**What role should be pinged for the commission?**')
       .setColor(config.colour);
   const c = await message.channel.send(embed);
-  
+
   await delay(500);
   message.channel.awaitMessages(message => message.content !== '', { maxMatches: 1, time: 30000, errors: ['time'] })
   .then(collected => {
-    let role = collected.first().content;
-      
-    const embed2 = new Discord.RichEmbed()
+    let role = message.channel.guild.roles.find(x => x.name == collected.first().content);
+
+    const embed22 = new Discord.RichEmbed()
       .setTitle('Commission Creation System')
       .setDescription('Please use the instuctions below to create the commission:\n\n**Please provide a __title__ for the commission.**\n\n')
-      .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}`)
+      .addField('Commission:', `__Role:__ ${role.name}`)
       .setColor(config.colour);
-    
-    c.edit(embed2);
-  })
+
+    c.edit(embed22);
     message.channel.awaitMessages(message => message.content !== '', { maxMatches: 1, time: 30000, errors: ['time'] })
     .then(collected => {
       let title = collected.first().content;
-      
+
       const embed2 = new Discord.RichEmbed()
         .setTitle('Commission Creation System')
         .setDescription('Please use the instuctions below to create the commission:\n\n**Please provide a __description__ for the commission.**\n\n')
-        .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}`)
+        .addField('Commission:', `__Role:__${role.name}\n__Title:__ ${title}`)
         .setColor(config.colour);
-      
+
       c.edit(embed2);
-      
+
       message.channel.awaitMessages(message => message.content !== '', { maxMatches: 1, time: 30000, errors: ['time'] })
       .then(collected => {
           const description = collected.first().content;
-        
+
           const embed3 = new Discord.RichEmbed()
             .setTitle('Commission Creation System')
             .setDescription('Please use the instuctions below to create the commission:\n\n**Please provide a __timeframe__ for the commission.**\n\n')
             .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}\n__Description:__ ${description}`)
             .setColor(config.colour);
-        
+
           c.edit(embed3);
-        
+
           message.channel.awaitMessages(message => message.content !== '', { maxMatches: 1, time: 30000, errors: ['time'] })
             .then(collected => {
                 const timeframe = collected.first().content;
@@ -68,83 +71,107 @@ module.exports.run = async (client, message, args) => {
 
                 message.channel.awaitMessages(message => message.content !== '', { maxMatches: 1, time: 30000, errors: ['time'] })
                   .then(collected => {
-                      const budget = collected.first().content;
+                      const details = collected.first().content;
 
-                      const embed5 = new Discord.RichEmbed()
+                      const embed44 = new Discord.RichEmbed()
                         .setTitle('Commission Creation System')
-                        .setDescription('Please use the instuctions below to create the commission:\n\n**Please let the client react below to confirm, once the client reacts the commission will be posted.**\n\n')
+                        .setDescription('Please use the instuctions below to create the commission:\n\n**Are there any extra details?**\n\n')
                         .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}\n__Description:__ ${description}\n__Timeframe:__ ${timeframe}\n__Budget:__ ${budget}`)
                         .setColor(config.colour);
 
-                      const embed6 = new Discord.RichEmbed()
-                        .setTitle('Commission Creation System')
-                        .setDescription('** **\n<a:tick:606612530378571776> **Commission Posted!**\n\n** **')
-                        .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}\n__Description:__ ${description}\n__Timeframe:__ ${timeframe}\n__Budget:__ ${budget}`)
-                        .setColor(config.colour);
-                  
-                      const embed7 = new Discord.RichEmbed()
-                        .setTitle('Commission Creation System')
-                        .setDescription('** **\n<a:cross:606618471786217485> **Client denied has denied the commission, a member of the Support Team will be with you shortly to seek out why.**\n\n** **')
-                        .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}\n__Description:__ ${description}\n__Timeframe:__ ${timeframe}\n__Budget:__ ${budget}`)
-                        .setColor(config.colour);
-                  
-                      c.edit(embed5);
-                  
-                      c.react('❌');
-                      c.react('✅');
+                      c.edit(embed44);
 
-                      const filter = (reaction, user) => {
-                        return user.id !== message.author.id && !user.bot &&(reaction.emoji.name === '✅' || reaction.emoji.name === '❌');
-                      };
+                      message.channel.awaitMessages(message => message.content !== '', { maxMatches: 1, time: 30000, errors: ['time'] })
+                        .then(async (collected) => {
+                            const budget = collected.first().content;
 
-                      const collector = c.createReactionCollector(filter, { maxMatches: 1, time: 86400000 });
+                            const embed5 = new Discord.RichEmbed()
+                              .setTitle('Commission Creation System')
+                              .setDescription('Please use the instuctions below to create the commission:\n\n**Please let the client react below to confirm, once the client reacts the commission will be posted.**\n\n')
+                              .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}\n__Description:__ ${description}\n__Timeframe:__ ${timeframe}\n__Budget:__ ${budget}\n__Any extra details?__${details}`)
+                              .setColor(config.colour);
 
-                      collector.on('collect', (reaction, reactionCollector) => {
-                        if (reaction.emoji.name === '✅') {
-                          c.edit(embed6)
-                        } else if (reaction.emoji.name === '❌') {
-                          c.edit(embed7)
+                            const embed6 = new Discord.RichEmbed()
+                              .setTitle('Commission Creation System')
+                              .setDescription('** **\n<a:tick:606612530378571776> **Commission Posted!**\n\n** **')
+                              .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}\n__Description:__ ${description}\n__Timeframe:__ ${timeframe}\n__Budget:__ ${budget}\n__Any extra details?__${details}`)
+                              .setColor(config.colour);
 
-                          var Achannel = client.channels.get('<channel id>') //commission channel
+                            const embed7 = new Discord.RichEmbed()
+                              .setTitle('Commission Creation System')
+                              .setDescription('** **\n<a:cross:606618471786217485> **Client denied has denied the commission, a member of the Support Team will be with you shortly to seek out why.**\n\n** **')
+                              .addField('Commission:', `__Role:__ ${role.name}\n__Title:__ ${title}\n__Description:__ ${description}\n__Timeframe:__ ${timeframe}\n__Budget:__ ${budget}\n__Any extra details?__${details}`)
+                              .setColor(config.colour);
 
-                          const embed8 = new Discord.RichEmbed()
-                          .setTitle('New Commission')
-                          .setDescription('** **\nNew commission, please react with ✅ to claim the commission.\n\n** **')
-                          .addField('Title:', `${title}`)
-                          .addField('Description:', `${description}`)
-                          .addField('Timeframe:', `${timeframe}`)
-                          .addField('Budget:', `${budget}`)
+                            c.edit(embed5);
 
-                          Achannel.send(`<@&${role.id}>`);
-                          let m = await Amessage.send(embed8);
+                            c.react('✅');
+                            await delay(100);
+                            c.react('❌');
 
-                          m.react('❌');
-                          m.react('✅');
 
-                          const collector = Amessage.createReactionCollector(reaction => reaction.emoji.name !== "");
-                          collector.on("collect", reaction => {
-                            if (reaction.emoji.name === '✅') {
-                              const embed9 = new Discord.RichEmbed()
-                              .setDescription(`Commission has been claimed by ${reaction.user.first().name}`);
-                              
-                              m.edit(embed9);
-                              
-                              message.channel.overwritePermissions(reaction.user.first().name, {
-                                VIEW_CHANNEL: true,
-                                SEND_MESSAGES: true
-                              })
+                            const filter = (reaction, user) => {
+                              return user.id == message.author.id && !user.bot &&(reaction.emoji.name === '✅' || reaction.emoji.name === '❌');
+                            };
 
-                              m.edit(embed9);
-                              
-                              const embed10 = new Discord.RichEmbed()
-                              .setDescription('A freelancer has claimed your commission.')
+                            const collector = c.createReactionCollector(filter, { maxMatches: 1, time: 86400000 });
 
-                              message.channel.send(embed10)    
-                            }
-                          })
-                        }
-                      });
+                            collector.on('collect', async (reaction, reactionCollector) => {
+                              if (reaction.emoji.name === '❌') {
+                                c.edit(embed7)
+                              } else if (reaction.emoji.name === '✅') {
+                                c.edit(embed6)
 
+                                var Achannel = client.channels.get('605775495077822474') //commission channel
+
+                                const embed8 = new Discord.RichEmbed()
+                                .setTitle('New Commission')
+                                .setDescription('** **\nPlease react with ✅ to claim the commission.\n\n** **')
+                                .addField('Title:', `${title}`, true)
+                                .addField('Timeframe:', `${timeframe}`, true)
+                                .addField('Budget:', `${budget}`, true)
+                                .addField('Description:', `${description}`, false)
+                                .addField('Any Extra Details?', `${description}`, true)
+                                .addBlankField(true)
+                                .addField('Ticket:', `<#${message.channel.id}>`, true);
+                                Achannel.send(`<@&${role.id}>`);
+                                let m = await Achannel.send(embed8);
+
+                                m.react('✅');
+                                await delay(100);
+                                m.react('❌');
+
+                                const filter2 = (reaction, user) => {
+                                  return !user.bot &&(reaction.emoji.name === '✅' || reaction.emoji.name === '❌');
+                                };
+
+                                const collector = m.createReactionCollector(filter2);
+                                collector.on("collect", reaction => {
+                                  if (reaction.emoji.name === '✅') {
+                                    const embed9 = new Discord.RichEmbed()
+                                    .setDescription(`Commission has been claimed by **user**!`);
+
+                                    message.channel.overwritePermissions(message.guild.defaultRole, {
+                                      VIEW_CHANNEL: true,
+                                      SEND_MESSAGES: true
+                                    })
+
+                                    m.edit(embed9);
+
+                                    const embed10 = new Discord.RichEmbed()
+                                    .setDescription('Your commission has been claimed by **user**!')
+
+                                    message.channel.send(embed10)
+                                  }
+                                })
+                              }
+                            });
+
+                        })
+                        .catch(collected => {
+                          message.channel.send(ended);
+                          console.log(collected);
+                        });
                   })
                   .catch(collected => {
                     message.channel.send(ended);
@@ -155,21 +182,23 @@ module.exports.run = async (client, message, args) => {
             message.channel.send(ended);
             console.log(collected);
           });
-        
+
       })
       .catch(collected => {
         message.channel.send(ended);
         console.log(collected);
       });
-      
-      
+
+
     })
     .catch(collected => {
       message.channel.send(ended);
       console.log(collected);
-    })
+    });
+
+  })
   .catch(collected => {
-    message.channel.send(ended);
+    message.channel.send(ended2);
     console.log(collected);
   });
 }
